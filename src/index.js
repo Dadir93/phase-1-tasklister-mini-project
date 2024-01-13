00edacb7fcfdc9bd99bedaad98416854b7dacf2c
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-01-14 02:21:41 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById('create-task-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const taskDescription = document.getElementById('new-task-description').value;
+
+      const newTaskElement = document.createElement('li');
+      newTaskElement.textContent = taskDescription;
+
+      document.getElementById('tasks').appendChild(newTaskElement);
+
+      const tasksListedDiv = document.getElementById('tasks-listed');
+      const newTaskListedElement = document.createElement('div');
+      newTaskListedElement.textContent = taskDescription;
+
+      tasksListedDiv.appendChild(newTaskListedElement);
+
+      document.getElementById('new-task-description').value = '';
+  });
 });
